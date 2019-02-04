687e0fcb1ef6f896b7ae7670609fe852348a074a
bs <bs@example.com>
2019-02-04 15:04:14 +0000

    switch server stack to spring boot wicket

--- drift-server/src/main/java/hello/world/graphql/DataFetchers.java
+++
@@ -1,7 +0,0 @@
-package hello.world.graphql;
-
-/**
- * Marker interface used to denote a particular class contains one or more {@link DataFetcher} annotated methods.
- */
-public interface DataFetchers {
-}
