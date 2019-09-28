a72a86d2c669efb32d14a7c16a62ea501c2030cc
bs <bs@example.com>
2019-09-28 12:42:44 +0000

    elasticsearch plugin initial

---
+++ drift-elasticsearch/drift-elasticsearch-springboot/src/main/java/io/drift/plugin/elasticsearch/DriftElasticSearchAutoConfig.java
@@ -0,0 +1,14 @@
+package io.drift.plugin.elasticsearch;
+
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = DriftElasticSearchAutoConfig.class)
+@AutoConfigurationPackage
+public class DriftElasticSearchAutoConfig {
+
+    public static final String ELASTICSEARCH_SUBSYSTEM_TYPE = "elasticsearch";
+
+}
